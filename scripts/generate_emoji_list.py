402db243f30e420a682467c9c8566ac96d6e5029
[MΛLTE] <maltem.hilpert@gmx.de>
2023-09-28 22:37:06 +0200

    Change storage method

--- scripts/generate_emoji_list.py
+++ scripts/generate_emoji_list.py
@@ -6,13 +6,15 @@ def main():
     print(os.listdir())
     try:
         root_dir = './fluentui-emoji/assets'
-        folder_dict = {"default":[], "color":[]}
+        # folder_dict = {"default":[], "color":[]}
+        arr = []
         for folder in sorted(os.listdir(root_dir)):
-            key = "default" if "3D" in os.listdir(os.path.join(root_dir, folder)) else "color"
-            folder_dict[key].append("_".join(folder.lower().split()))
+            key = "" if "3D" in os.listdir(os.path.join(root_dir, folder)) else "-color"
+            # folder_dict[key].append("_".join(folder.lower().split()))
+            arr.append("_".join(folder.lower().split())+key)
             
         with open("./cdn/fluentui-emoji-list.json", "w") as f:
-            json.dump(folder_dict, f, ensure_ascii=False)
+            json.dump(arr, f, ensure_ascii=False)
         return 0
 
     except Exception as e:
