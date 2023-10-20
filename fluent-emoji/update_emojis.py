08a51964aab3edd89f58be04afbdaf5c24ddc335
[MΛLTE] <maltem.hilpert@gmx.de>
2023-10-20 16:10:24 +0200

    Updates

--- fluent-emoji/update_emojis.py
+++ fluent-emoji/update_emojis.py
@@ -58,6 +58,7 @@ def try_varients(emoji):
 
 def save_images(img_path, output_path):
   extension = os.path.splitext(img_path)
+  print(extension)
   if (extension == '.svg'):
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     shutil.copy(img_path, f'{output_path}.svg')
